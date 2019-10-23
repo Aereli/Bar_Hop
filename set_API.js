a5434e0306ff047540efb7ee7ccc186133892f9b
williamkennedy <wkennedy8@yahoo.com>
2019-10-23 17:41:54 -0400

    add API request ranges

--- set_API.js
+++ set_API.js
@@ -1,7 +1,45 @@
 const axios = require('axios')
 
-axios.get('https://api.yelp.com/v3/businesses/search?term=bar&location=miami', {
-  headers: {
-    Authorization: 'Bearer cYFpFbXF-c4gssR0CO_0hSek9SsPJVfPAS2QGUSM_Z5JrTDz01SC_EAqMro8tbIxDjdowtCsI4WgsZwAxE5ka1dXupktHbPaC84vZknCIn858gqYMbHQWkqOmzmvXXYx'
+const apiKey = 'cYFpFbXF-c4gssR0CO_0hSek9SsPJVfPAS2QGUSM_Z5JrTDz01SC_EAqMro8tbIxDjdowtCsI4WgsZwAxE5ka1dXupktHbPaC84vZknCIn858gqYMbHQWkqOmzmvXXYx'
+
+const getAllPages = async () => {
+  const requests = []
+  let failures = []
+  const bars = []
+
+  const url = pageNum => `https://api.yelp.com/v3/businesses/search?term=bar&location=miami&limit=50&offset=${pageNum*50}`
+
+  const pageRequest = pageNum => (
+    axios.get(url(pageNum), { headers: { Authorization: `Bearer ${apiKey}` }})
+      .catch(_ => {
+        failures.push(pageNum)
+        return undefined
+      })
+  )
+
+  const tryToGetData = async requests => {
+    (await Promise.all(requests))
+        .filter(response => !!response) // ignore ones that return undefined because of error
+        .map(response => response.data.businesses.map(business => {
+          const { name, image_url } = business
+          return { name, image_url }
+        }))
+        .forEach(bar => bars.push(bar))
   }
-}).then(response => console.log(response.data)) 
+
+  for (let pageNum = 0; pageNum < 20; pageNum++){
+    requests.push(pageRequest(pageNum))
+  }
+
+  await tryToGetData(requests)
+
+  do {
+    const failedPageNums = [...failures]
+    failures = []
+    await tryToGetData(failedPageNums.map(pageNum => pageRequest(pageNum)))
+  }while(failures.length)
+
+  console.log(bars)
+}
+
+getAllPages()
